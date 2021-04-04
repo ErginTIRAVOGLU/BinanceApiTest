66ee46e336c3f3fde9fe58a10871958511e1fd87
Ergin <ergintiravoglu@msn.com>
2021-04-04 14:22:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -186,6 +186,8 @@ $json = json_decode($response,true);
 
 </head>
 <body>
+ <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
+
     <div>
     <?php
         for ($i=0; $i < count($fiyatlar) ; $i++) { 
